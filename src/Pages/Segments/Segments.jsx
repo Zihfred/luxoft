5044c3c624b222c984478fd0d6b2046e307d4d0e
zihfred <Lichkingblizzard@gmail.com>
2023-12-08 17:42:50 +0200

    added segments page

---
+++ src/Pages/Segments/Segments.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 16px;
+`;
+
+const Segments = () => {
+  return (
+    <Wrapper>
+    </Wrapper>
+  );
+};
+
+export default Segments;
