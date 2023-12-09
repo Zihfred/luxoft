8c5f29be5b4283e73f33cfc09cc10cb161c7314b
zihfred <Lichkingblizzard@gmail.com>
2023-12-09 14:03:21 +0200

    changed api path

--- src/api.js
+++ src/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const getAllUsers = () =>
   axios({
     method: "get",
-    url: "https://alpha.deadalice.xyz:4000/api/users",
+    url: "http://alpha.deadalice.xyz:4000/api/users",
   }).then((res) => res.data.users);
